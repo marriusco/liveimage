0c0bc68c9f25eac816bd531ad78f4e98816044b8
brlochin <octavian.chincisan@flextronics.com>
2016-10-01 16:52:33 -0400

    added docs folder

---
+++ CMakeFiles/liveimage.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marius/APPS/GITHUB/REPO/liveimage/jpeger.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/jpeger.cpp.o"
+  "/home/marius/APPS/GITHUB/REPO/liveimage/main.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/main.cpp.o"
+  "/home/marius/APPS/GITHUB/REPO/liveimage/outfilefmt.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/outfilefmt.cpp.o"
+  "/home/marius/APPS/GITHUB/REPO/liveimage/pnger.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/pnger.cpp.o"
+  "/home/marius/APPS/GITHUB/REPO/liveimage/sock.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/sock.cpp.o"
+  "/home/marius/APPS/GITHUB/REPO/liveimage/sockserver.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/sockserver.cpp.o"
+  "/home/marius/APPS/GITHUB/REPO/liveimage/v4ldevice.cpp" "/home/marius/APPS/GITHUB/REPO/liveimage/CMakeFiles/liveimage.dir/v4ldevice.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/liveimage.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/liveimage.dir/main.cpp.o"
+  "CMakeFiles/liveimage.dir/jpeger.cpp.o"
+  "CMakeFiles/liveimage.dir/outfilefmt.cpp.o"
+  "CMakeFiles/liveimage.dir/pnger.cpp.o"
+  "CMakeFiles/liveimage.dir/sock.cpp.o"
+  "CMakeFiles/liveimage.dir/sockserver.cpp.o"
+  "CMakeFiles/liveimage.dir/v4ldevice.cpp.o"
+  "liveimage.pdb"
+  "liveimage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/liveimage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
